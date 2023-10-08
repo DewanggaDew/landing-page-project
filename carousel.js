2276a2a965054ad6656b2f4af91f148aaf0f2339
DewanggaDew <dewangga.indera@gmail.com>
2023-10-08 14:01:36 +0800

    Got Carousel Working

--- carousel.js
+++ carousel.js
@@ -1,43 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const carouselSlide = document.querySelector(".carousel-slide");
-  const carouselImages = document.querySelectorAll(".carousel-slide img");
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel-btn-right");
+const prevButton = document.querySelector(".carousel-btn-left");
 
-  // Buttons
-  const prevBtn = document.querySelector(".carousel-btn-left");
-  const nextBtn = document.querySelector(".carousel-btn-right");
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-  // Counter
-  let counter = 1;
-  const size = carouselImages[0].clientWidth;
+const setSlidePositon = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePositon);
 
-  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
 
-  // Button Listeners
+//when clicked left, move slides to left
+prevButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
 
-  nextBtn.addEventListener("click", () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-  });
+  moveToSlide(track, currentSlide, prevSlide);
+});
 
-  prevBtn.addEventListener("click", () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-  });
+//when clicked right, move slides to right
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
 
-  carouselSlide.addEventListener("transitionend", () => {
-    if (carouselImages[counter].id === "lastClone") {
-      carouselSlide.style.transition = "none";
-      counter = carouselImages.length - 2;
-      carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-    }
-    if (carouselImages[counter].id === "firstClone") {
-      carouselSlide.style.transition = "none";
-      counter = carouselImages.length - counter;
-      carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-    }
-  });
+  moveToSlide(track, currentSlide, nextSlide);
 });
